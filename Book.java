d759ba263bf12fe57aa5b2b15395a6336aff5bef
Savannah-Lu <savannahlut@nycstudents.net>
2025-05-16 13:14:37 +0000

    book constructor

--- Book.java
+++ Book.java
@@ -1,2 +1,9 @@
 public class Book{
+    private String genre, title, author;
+    
+    public Book(String t, String a, String g){
+        title = t;
+        author = a; 
+        genre = g;
+    }
 }
